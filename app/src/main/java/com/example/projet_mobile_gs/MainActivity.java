feb6bbedf376b74df551af0c3fb0fa166fee7eed
Your Name <paulespinosa111@gmail.com>
2022-12-03 17:40:54 -0500

    Ajout des deux premières interfaces

--- app/src/main/java/com/example/projet_mobile_gs/MainActivity.java
+++ app/src/main/java/com/example/projet_mobile_gs/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.projet_mobile_gs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_entrepot);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.appbar_color)));
+
+        // Prise en compte du bouton et lancement de l'intent add_entrepot
+        addButton = findViewById(R.id.boutonAdd);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), add_entrepot.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
