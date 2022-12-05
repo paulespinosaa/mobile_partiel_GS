34ee06c76fca65cfacd1714c40dc3f26bfbfc6eb
copiright <antoine.munsch@groupe-esigelec.org>
2022-12-04 23:43:27 -0500

    delete et modify produit

---
+++ app/src/main/java/com/example/projet_mobile_gs/EditProduit.java
@@ -0,0 +1,76 @@
+package com.example.projet_mobile_gs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
+
+public class EditProduit extends AppCompatActivity {
+
+    private ImageButton arrowBack;
+    private EditText nomProduitInput;
+    private EditText quantiteProduit;
+    private ImageButton ajouterProduit;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_item);
+        int entrepotID = getIntent().getIntExtra("entrepotID", 0);
+        String nomProduit = getIntent().getStringExtra("entrepotID");
+        String quantiteProduitAncien = getIntent().getStringExtra("quantite");
+
+        nomProduitInput = findViewById(R.id.nomProduitOut);
+        quantiteProduit = findViewById(R.id.quantiteProduitOut);
+        ajouterProduit = (ImageButton) findViewById(R.id.boutonSave2);
+
+        Realm realm = Realm.getDefaultInstance();
+        RealmResults<Produit> listProduit = realm.where(Produit.class).findAll();
+        Produit newProduit = new Produit();
+
+        for (Produit produit : listProduit) {
+            if(produit.getNomProduit().equals(nomProduit)){
+                newProduit = produit;
+            }
+        }
+
+        nomProduitInput.setText(newProduit.getNomProduit());
+        quantiteProduit.setText(newProduit.getQuantite());
+
+        Realm.init(getApplicationContext());
+
+        arrowBack = findViewById(R.id.arrowBack);
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+        Produit finalNewProduit = newProduit;
+        ajouterProduit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nomPdt = nomProduitInput.getText().toString();
+                int qtte = Integer.valueOf( quantiteProduit.getText().toString());
+
+                realm.beginTransaction();
+                finalNewProduit.deleteFromRealm();
+                Produit produit =  realm.createObject(Produit.class);
+                produit.setNomProduit(nomPdt);
+                produit.setQuantite(qtte);
+                produit.setIdEntrepot(entrepotID);
+                realm.commitTransaction();
+                finish();
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/projet_mobile_gs/MyAdapterProduit.java
+++ app/src/main/java/com/example/projet_mobile_gs/MyAdapterProduit.java
@@ -1,14 +1,21 @@
 package com.example.projet_mobile_gs;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class MyAdapterProduit extends RecyclerView.Adapter<MyAdapterProduit.MyViewHolder2> {
@@ -29,7 +36,40 @@ public class MyAdapterProduit extends RecyclerView.Adapter<MyAdapterProduit.MyVi
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder2 holder, int position) {
+            Produit produit = produitList.get(position);
+            holder.nomProduitOut.setText(produit.getNomProduit());
+            holder.quantiteProduitOut.setText(produit.getQuantite());
 
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                PopupMenu menu =  new PopupMenu(context,v);
+                menu.getMenu().add("Delete");
+                menu.getMenu().add("Edit");
+                menu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        if(item.getTitle().equals("Delete")){
+                            Realm realm = Realm.getDefaultInstance();
+                            realm.beginTransaction();
+                            produit.deleteFromRealm();
+                            realm.commitTransaction();
+                        }
+                        if(item.getTitle().equals("Edit")){
+                            Intent intent = new Intent(context.getApplicationContext(), EditEntrepotNameActivity.class);
+                            intent.putExtra("nomProduit",produit.getNomProduit());
+                            intent.putExtra("quantite",produit.getQuantite());
+                            intent.putExtra("entrepotID", produit.getIdEntrepot());
+                            intent.setFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+                            context.getApplicationContext().startActivity(intent);
+                        }
+                        return true;
+                    }
+                });
+                menu.show();
+                return true;
+            }
+        });
     }
 
     @Override
